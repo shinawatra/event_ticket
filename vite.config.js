de6c08de9b68ac113c626ccc2d10399928ceb8b3
shinawatra <shinawatra94@gmail.com>
2025-03-12 12:33:13 +0100

    adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/event_ticket/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 });
+
+//*/event_ticket/*
